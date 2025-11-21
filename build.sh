bcc287fb21389dc7c9f22fa1d5f3664d4a9c0a5f
ByWerton <145929596+ByWerton@users.noreply.github.com>
2025-11-21 22:02:01 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
-git clone https://github.com/emscripten-core/emsdk.git
-cd emsdk
-git pull
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh
-cd ..
-emcc motor_core.cpp -o motor_core.js -s WASM=1 -s SINGLE_FILE=1 -s EXPORTED_FUNCTIONS='["_RunScript", "_InitializeLunXStudio", "_CreateNewBlockFromUI"]'
-if [ -f "motor_core.js" ]; then
-exit 0
+
+# =========================================================
+# Emscripten SDK (emsdk) Kurulumu
+# =========================================================
+# emsdk repo'sunu klonla veya güncelle
+if [ ! -d "emsdk" ]; then
+    git clone https://github.com/emscripten-core/emsdk.git
 else
-exit 1
+    (cd emsdk && git pull)
 fi
+
+# 'latest' SDK'yı indir ve kur
+./emsdk/emsdk install latest
+./emsdk/emsdk activate latest
+
+# EMSDK ortam değişkenlerini ayarla
+source "./emsdk/emsdk_env.sh"
+
+# =========================================================
+# Derleme Komutu
+# =========================================================
+
+# motor_core.cpp dosyasını derle
+# Gerekli Bayraklar:
+# 1. --js-library js_api.js: C++'tan çağrılan JS fonksiyonlarının tanımlarını (stubs) sağlar.
+# 2. -s EXPORTED_FUNCTIONS: C-style olarak dışa aktarılacak fonksiyonları belirtir. (Önek olarak "_" unutulmamalıdır.)
+# 3. --bind: Emscripten C++ bindings (embind) kullanımını etkinleştirir.
+
+emcc motor_core.cpp \
+    --js-library js_api.js \
+    -o motor.js \
+    -s MODULARIZE=1 \
+    -s EXPORT_ES6=1 \
+    -s EXPORT_NAME=Module \
+    -s INITIAL_MEMORY=32MB \
+    -s EXPORTED_FUNCTIONS='_RunScript,_InitializeEngine,_CreateNewBlockFromUI' \
+    --bind \
+    -O2 
